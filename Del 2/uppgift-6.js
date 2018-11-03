76b2b97010f1ed6c67b83837815c57b7ded275f2
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2018-11-03 10:51:52 +0100

    Felmeddelande

--- Del 2/uppgift-6.js
+++ Del 2/uppgift-6.js
@@ -1,29 +1,40 @@
 // Läser in ett heltal och lagrar värdet i variabeln helTal.
-let helTal = prompt("Skriv ett heltal");
+let helTal = prompt("Skriv ett heltal mellan 1 och 1000");
 
 // Funktionen räknar ut fakultet av ett valfritt heltal mellan 1 och 1000.
 function factorial(nummer){
     let fakultet = 1;
     let x = 1;
     let meddelandet;
-
-    /* if-else satsen beräknar fakulteten om parameter "nummer" är ett heltal mellan 1 och 1000 
-    och tilldelar resultaten till variabeln "meddelandet". 
-    Annars tilldelar ett felmeddelande till variabeln "meddelandet"*/ 
-    if((nummer >= 1) && (nummer <= 1000) && (Number.isInteger(nummer)) && (!isNaN(nummer))){
+ 
+    if(nummer >= 1 && nummer <= 1000 && Number.isInteger(+nummer)){
         
-        // while-loop som beräknar fakulteten av "nummer".
+        // while-loop som beräknar fakulteten av nummer.
         while(x <= nummer){
             fakultet *= x;
             x++;
-        } 
-        meddelandet = "<h1>Fakulteten av talet " + nummer + " är " + fakultet + "</h1>"; 
+        }
+
+        if(fakultet === Infinity){
+        
+            meddelandet = "<h1>Fel! Det går inte att räkna ut fakulteten av talet " + nummer + "</h1>";   
+            
+        } else {
+            
+            meddelandet = "<h1>Fakulteten av talet " + nummer + " är " + fakultet + "</h1>";
+            
+        }
+        
+    } else if(nummer === null || isNaN(nummer)){
+        
+        meddelandet = "<h1>Fel inmatning!</h1>";
 
     } else {
-
+            
         meddelandet = "<h1>" + nummer + " är inte ett heltal mellan 1 och 1000</h1>";
 
     }
+    
     
     // Skriver ut ett meddelande på websidan. 
     document.write(meddelandet);
